d39962c9fc6a4aba3cf25d1e9183612092838640
Bakijazzman <obafemiolabayo@gmail.com>
2023-09-04 13:08:14 -0700

    cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - find a loop in a list
+ * @list: points to the start of the list
+ * Return: 0 if no cycle, 1 if cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slug, *hare;
+	
+	if (!list)
+		return (0);
+	slug = list;
+	hare = list;
+	for (; slug && hare && hare->next; )
+	{
+		hare = hare->next->next;
+		slug = slug->next;
+		if (slug == hare)
+			return (1);
+	}
+	return (0);
+}
