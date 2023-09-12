62752185d25f5b106d2d82982cf619e9d7ec83f0
Bakijazzman <obafemiolabayo@gmail.com>
2023-09-11 18:51:53 -0700

    length

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        sentence[i] = None
+    i = 0
+    length = len(sentence)
+    result = (length, sentence[i])
+
+    return result
