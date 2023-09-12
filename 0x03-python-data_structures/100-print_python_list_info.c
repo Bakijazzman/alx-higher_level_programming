5e62b612e87f46d2fb7a2798869a065e6d06833e
Bakijazzman <obafemiolabayo@gmail.com>
2023-09-11 19:12:39 -0700

    betty

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -4,7 +4,6 @@
 
 /**
 * print_python_list_info - prints some basic info about pytohn list
-* @p: python object
 **/
 
 void print_python_list_info(PyObject *p)
@@ -15,7 +14,6 @@ void print_python_list_info(PyObject *p)
 
 	printf("[*] Size of the Python List = %li\n", size);
 	printf("[*] Allocated = %li\n", obj->allocated);
-	//for (i = 0; i < size; i++)
 	while (i < size)
 	{
 		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
