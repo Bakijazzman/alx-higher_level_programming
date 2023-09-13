78e219ecfddcf69d02dac6e11ad25c5c38362501
Bakijazzman <obafemiolabayo@gmail.com>
2023-09-13 15:40:03 -0700

    formatted string

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     new = sorted(a_dictionary)
     for mem in new:
-        print(f"{mem} : {a_dictionary[mem]}")
+        print("{} : {}".format(mem, a_dictionary[mem]))
