451858375ebf7897783df58e05a18bb1c7345f17
Bakijazzman <obafemiolabayo@gmail.com>
2023-09-25 11:27:04 -0700

    print to standard error

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as msg:
+        print("Exception: {}".format(msg), file=sys.stderr)
+        return False
