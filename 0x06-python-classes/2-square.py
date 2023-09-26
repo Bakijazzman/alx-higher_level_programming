7a965c50a5c473e9aa8a7ca2273ab67fcac39561
Bakijazzman <obafemiolabayo@gmail.com>
2023-09-26 13:46:52 -0700

    pycode style

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -15,6 +15,7 @@ class Square:
                 TypeError: size must be an integer
                 ValueError: size must be >= 0
         """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
