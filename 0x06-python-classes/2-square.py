c49289ca2cda92507cd7a5100544a14f8754451c
Bakijazzman <obafemiolabayo@gmail.com>
2023-09-26 13:13:44 -0700

    isinstance

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class module"""
+
+
+class Square:
+    """Square Class"""
+
+    def __init__(self, size=0):
+        """class instance"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size of int must be >= 0")
+        self.__size = size
