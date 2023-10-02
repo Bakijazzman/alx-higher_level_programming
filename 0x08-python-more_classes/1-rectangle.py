a9bb1ae91e9e07a46d5bc7b66f16baa5bce17f9f
Bakijazzman <obafemiolabayo@gmail.com>
2023-10-02 13:07:17 -0700

    setting attributes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""A class that defines a rectangle"""
+
+
+class Rectangle:
+    """An empty class with no methods or attributes"""
+    def __init__(self, width=0, height=0):
+        """Initializing the class
+        Args:
+            width: represents the width of the rectangle
+            height: represents the height of the rectangle
+        Raises:
+            TypeError: if input is not an integer
+            ValueError: if input is less than 0
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self, value):
+        """returns attributes to the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets width attribute"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("Width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self, value):
+        """retturn the height attribute """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets height attributes"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
