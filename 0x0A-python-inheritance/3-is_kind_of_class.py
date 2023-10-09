4b005b87398110cfa3cb30528d3ea6f5529ed937
Bakijazzman <obafemiolabayo@gmail.com>
2023-10-09 23:46:58 +0100

    is kind

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Imports Module Here """
+
+
+def is_kind_of_class(obj, a_class):
+    """checks kind of class
+    Args:
+    obj: object given
+    a_class: class given
+    """
+    return isinstance(obj, a_class)
