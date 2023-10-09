d2a7a31085419a5e8aa4a39c9f9061c449f67795
Bakijazzman <obafemiolabayo@gmail.com>
2023-10-10 00:02:38 +0100

    more base  geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Imports module here """
+
+
+class BaseGeometry:
+    """an empty class """
+    def area(self):
+        """raises an exception """
+        raise Exception("area() is not implemented")
