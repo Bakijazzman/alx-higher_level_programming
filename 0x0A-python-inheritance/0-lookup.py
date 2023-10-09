34c468a3596e7930585d06294dc587f9b8819fa1
Bakijazzman <obafemiolabayo@gmail.com>
2023-10-09 23:15:21 +0100

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """function to look up attributes and modules
+        Args:
+        obj: the object to lookup
+        Return: list of available attributes and modules
+    """
+    return dict(obj)
