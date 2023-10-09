5013ba3edc1bbd0a1c21ac1f6b5b6d52da2c1ddb
Bakijazzman <obafemiolabayo@gmail.com>
2023-10-10 00:17:32 +0100

    base geometry

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usrbin/python3
+"""Imports module here """
+
+class BaseGeometry:
+    """an empty class """
+    def area(self):
+        """raises an exception """
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """validates inputs
+        Args:
+        name: a string to be checked
+        value: must be greater than 0
+        """
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
