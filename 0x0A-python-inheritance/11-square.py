e88dfa8134a3311e245ee81c764adaf2cfc10636
Bakijazzman <obafemiolabayo@gmail.com>
2023-10-10 01:02:28 +0100

    square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Import modules here """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class that inherits from Rectangle"""
+    def __init__(self, size):
+        """initializing attributes """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """function that calculates area"""
+        return self.__size * self.__size
+    
+    def __str__(self):
+        """func to return the square description"""
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
