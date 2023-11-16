6881417b4d4f32376beeb986bef2854dbde6ce2b
Bakijazzman <obafemiolabayo@gmail.com>
2023-11-16 22:28:52 +0100

    count

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@ SELECT g.name AS genre, COUNT(t.genre_id) AS number_of_shows
 FROM tv_genres AS g
 INNER JOIN tv_shows_genres AS t
 ON g.id = t.genre_id
-GROUP BY g.name
-ORDER BY number_of_shows DESC;
+GROUP BY t.genre_id
+ORDER BY number_of_shows DESC, g.id ASC;
