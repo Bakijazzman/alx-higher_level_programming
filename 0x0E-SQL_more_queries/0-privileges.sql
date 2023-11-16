5b4a2adcd230c6c75ad8f907d7d5aa31cd1b082c
Bakijazzman <obafemiolabayo@gmail.com>
2023-11-16 14:37:49 +0100

    grants

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- show all user priveleges
-SHOW GRANTS FOR user_0d_1;
-SHOW GRANTS FOR user_0d_2;
+-- show all user privileges
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
