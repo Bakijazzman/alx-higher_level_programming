fdec049e02d8ba5f7b4665531192e808c6860c28
Bakijazzman <obafemiolabayo@gmail.com>
2023-11-16 18:57:21 +0100

    fix syntax

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- craetes table force_name
-CREATE TABLE IF NOT EXIST force_name (id INT, name VARCHAR(256) IS NOT NULL);
+CREATE TABLE IF NOT EXIST force_name (id INT, name VARCHAR(256) NOT NULL);
