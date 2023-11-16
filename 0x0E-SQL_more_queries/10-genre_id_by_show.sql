ba96eb8dcaa3ff13200459116bb8ad16ad97c337
Bakijazzman <obafemiolabayo@gmail.com>
2023-11-16 21:59:56 +0100

    genre and shows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- List all shows contained in the database that have at least one genre
-SELECT tv.title, gen.genre_id
-FROM tv_shows AS tv
-INNER JOIN tv_show_genres AS gen
-ON tv.shows.id = gen.show_id
-ORDER BY tv.title, gen.genre_id;
+SELECT t.title, g.genre_id
+FROM tv_shows AS t
+INNER JOIN tv_show_genres AS g
+ON t.shows.id = g.show_id
+ORDER BY t.title, g.genre_id;
