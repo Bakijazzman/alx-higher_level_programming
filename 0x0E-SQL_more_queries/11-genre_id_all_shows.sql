cdff1acf86df1e291e5866f895379f64c9d3e7cc
Bakijazzman <obafemiolabayo@gmail.com>
2023-11-16 22:03:32 +0100

    left joiin

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- List all shows contained in the database that have at least one genre
+SELECT t.title, g.genre_id
+FROM tv_shows AS t
+LEFT JOIN tv_show_genres AS g
+ON t.id = g.show_id
+ORDER BY t.title, g.genre_id;
