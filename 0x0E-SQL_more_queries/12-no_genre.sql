f7c76abc87c6cbf431f7b65156d8ae23f0ca1ec2
Bakijazzman <obafemiolabayo@gmail.com>
2023-11-16 22:21:58 +0100

    number of shows

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,5 +3,5 @@ SELECT t.title, g.genre_id
 FROM tv_shows AS t
 LEFT JOIN tv_show_genres AS g
 ON t.id = g.show_id
-WHERE g,genre_id IS NULL
+WHERE g.genre_id IS NULL
 ORDER BY t.title, g.genre_id;
---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+--displays number of shows linked to each
+SELECT g.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres AS g
+INNER JOIN tv_shows_genres AS t ON g.id = t.genre_id
+GROUP BY g.name
+ORDER BY number_of_shows DESC;
