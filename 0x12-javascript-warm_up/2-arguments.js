7fdf1c812ce611c64089a1cc87cc2b52d9b8c19b
Bakijazzman <obafemiolabayo@gmail.com>
2023-12-11 13:07:43 +0100

    fix condition

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,6 @@ if (process.argv.length < 3) {
   console.log('No arguments');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
-} else if {
+} else if (process.argv.length > 3) {
   console.log('Arguments found');
 }
