e5eb45cddfe953ecbecb8c0aaff535002e7850d1
Bakijazzman <obafemiolabayo@gmail.com>
2023-12-13 00:48:13 +0100

    classes methods in js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    let j = 0;
+    const first = this.height;
+    const second = this.width;
+    while (i < first) {
+      let show = '';
+      j = 0;
+      while (j < second) {
+        show += 'X';
+        j++;
+      }
+      console.log(show);
+      i++;
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+module.exports = Rectangle;
