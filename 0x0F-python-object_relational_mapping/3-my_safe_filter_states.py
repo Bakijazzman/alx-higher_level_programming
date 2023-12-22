4aaf500bd023de535a220c1a687dcd8d6d9926d1
Bakijazzman <obafemiolabayo@gmail.com>
2023-12-22 23:13:28 +0100

    same

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    Import Modules Here
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+        this functions gets arguments passed in
+    """
+    db_username = argv[1]
+    db_password = argv[2]
+    db_name = argv[3]
+    sort_by = argv[4]
+    db_port = 3306
+    db_host = "localhost"
+
+    connector = MySQLdb.connect(host=db_host, port=db_port,
+                                user=db_username, passwd=db_password,
+                                db=db_name, charset="utf8")
+    cursor = connector.cursor()
+    query = "SELECT * FROM states WHERE name = '{}'"
+    passed = query.format(sort_by)
+    cursor.execute(passed)
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1] == sort_by:
+            print(row)
+    cursor.close()
+    connector.close()
