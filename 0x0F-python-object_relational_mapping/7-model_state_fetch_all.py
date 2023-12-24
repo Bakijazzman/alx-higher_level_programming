1217b39aed41daacd250268e989d16a1e798ea30
Bakijazzman <obafemiolabayo@gmail.com>
2023-12-24 08:27:48 +0100

    sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+    Import Modules Here
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+
+def main():
+    """
+        this functions gets arguments passed in
+    """
+    db_username = argv[1]
+    db_password = argv[2]
+    db_name = argv[3]
+    db_host = "localhost"
+
+    try:
+        engine = create_engine("mysql+mysqldb://{}:{}@{}/{}"
+                               .format(
+                                   db_username,
+                                   db_password,
+                                   db_host,
+                                   db_name),
+                               pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+        session = Session(engine)
+        states = session.query(State).order_by(State.id).all()
+        for state in states:
+            print(f"{state.id}: {state.name}")
+        session.close()
+    except Exception:
+        pass
+
+
+if __name__ == "__main__":
+    main()
