3644cc43f2b34d8862f46a2b4c3b9b13ff0b4ac4
Bakijazzman <obafemiolabayo@gmail.com>
2024-01-11 09:26:14 +0100

    error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Import Modules Here """
+from sys import argv
+from urllib.error import HTTPError
+from urllib.request import urlopen, Request
+
+
+if __name__ == "__main__":
+    try: 
+        with urlopen(Request(argv[1])) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except HTTPError as exception:
+        print("Error code:", exception.code)
