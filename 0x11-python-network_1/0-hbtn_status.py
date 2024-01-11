52a190f2c18a3ec73aafb5dc2fdec7e6516a6c16
Bakijazzman <obafemiolabayo@gmail.com>
2024-01-11 08:18:33 +0100

    code fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Import modules here"""
-import urllib.request as request
+"""Fetches https://alx-intranet.hbtn.io/status."""
+import urllib.request
 
 
 if __name__ == "__main__":
-    request1 = request.Request("https://alx-intranet.hbtn.io/status")
-    with request.urlopen(request1) as response:
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
         body = response.read()
-        print("Body response")
+        print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode('utf-8')))
